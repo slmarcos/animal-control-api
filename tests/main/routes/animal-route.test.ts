bad2eac6759acd61d113120a9359f1d8ab76cf50
Marcos Lopes <sl.marcos15@hotmail.com>
2021-03-16 23:49:20 -0300

    ✅ ensure SaveAnimal route returns 400 if an animal attribute not send

---
+++ tests/main/routes/animal-route.test.ts
@@ -0,0 +1,39 @@
+import app from '@/main/configs/app'
+
+import { MongoDbAnimalModel } from '@/infra/db/models'
+import { MongoHelper } from '@/infra/db/helpers'
+
+import request from 'supertest'
+import faker from 'faker'
+
+const MONGO_URL = process.env.MONGO_URL as string
+
+describe('Animal Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    await MongoDbAnimalModel.deleteMany({})
+  })
+
+  describe('[POST] /animal', () => {
+    test('Should return 400 and missing param error if an animal attribute not send', async () => {
+      const mockRequest = {
+        age: faker.random.number(99),
+        type: faker.random.word(),
+        weight: faker.random.float(1000)
+      }
+      await request(app)
+        .post('/api/animal')
+        .send(mockRequest)
+        .expect(400, {
+          error: 'Missing param: name'
+        })
+    })
+  })
+})
