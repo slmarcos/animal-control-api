15e956d60facb5d57f4d6c7484ee19c508a38796
Marcos Lopes <sl.marcos15@hotmail.com>
2021-03-17 19:53:37 -0300

    ✅ add tests to LoadAnimalById route

--- tests/main/routes/animal-route.test.ts
+++ tests/main/routes/animal-route.test.ts
@@ -23,7 +23,7 @@ describe('Animal Routes', () => {
   })
 
   describe('[POST] /animal', () => {
-    test('Should returns status code 400 and missing param error if an animal attribute not send', async () => {
+    test('[POST] Should returns status code 400 and missing param error if an animal attribute not send', async () => {
       const mockRequest = {
         age: 12,
         type: faker.random.word(),
@@ -37,7 +37,7 @@ describe('Animal Routes', () => {
         })
     })
 
-    test('Should returns status code 200 and AnimalModel on success', async () => {
+    test('[POST] Should returns status code 200 and AnimalModel on success', async () => {
       const mockRequest = {
         name: faker.name.findName(),
         age: 12,
@@ -52,7 +52,7 @@ describe('Animal Routes', () => {
   })
 
   describe('[PUT] /animal/:animalId', () => {
-    test('Should returns status code 400 and missing param error if not send all animal attributes', async () => {
+    test('[PUT] Should returns status code 400 and missing param error if not send all animal attributes', async () => {
       const animalId = mongoose.Types.ObjectId().toHexString()
       const mockRequest = {
         age: 12,
@@ -67,7 +67,7 @@ describe('Animal Routes', () => {
         })
     })
 
-    test('Should returns noContent if animalId not exists', async () => {
+    test('[PUT] Should returns noContent if animalId not exists', async () => {
       const animalId = mongoose.Types.ObjectId().toHexString()
       const mockRequest = {
         name: faker.name.firstName(),
@@ -81,7 +81,7 @@ describe('Animal Routes', () => {
         .expect(204)
     })
 
-    test('Should returns status code 200 and updated AnimalModel on success', async () => {
+    test('[PUT] Should returns status code 200 and updated AnimalModel on success', async () => {
       const mockAnimal = {
         name: faker.name.firstName(),
         age: 12,
@@ -103,6 +103,44 @@ describe('Animal Routes', () => {
           age: mockRequest.age,
           type: mockRequest.type,
           weight: mockRequest.weight
+        })
+    })
+  })
+
+  describe('[GET] /animal/:animalId', () => {
+    test('[GET] Should returns status code 400 and invalid param error if an invalid animalId is provided', async () => {
+      const animalId = faker.random.uuid()
+      await request(app)
+        .get(`/api/animal/${animalId}`)
+        .expect(400, {
+          error: 'Invalid param: animalId'
+        })
+    })
+
+    test('[GET] Should returns noContent if animalId not existss', async () => {
+      const animalId = mongoose.Types.ObjectId().toHexString()
+      await request(app)
+        .get(`/api/animal/${animalId}`)
+        .expect(204)
+    })
+
+    test('[GET] Should returns status code 200 and AnimalModel on success', async () => {
+      const mockAnimal = {
+        name: faker.name.firstName(),
+        age: 12,
+        type: faker.random.word(),
+        weight: 100
+      }
+      const animal = await MongoDbAnimalModel.create(mockAnimal)
+      const animalId: string = animal.id
+      await request(app)
+        .get(`/api/animal/${animalId}`)
+        .expect(200, {
+          id: animalId,
+          name: mockAnimal.name,
+          age: mockAnimal.age,
+          type: mockAnimal.type,
+          weight: mockAnimal.weight
         })
     })
   })
